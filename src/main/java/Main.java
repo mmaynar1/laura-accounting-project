4b5e6d4df36238cb091aff79a0934c6bad1969b0
MSI\mitchm <mmaynar1@gmail.com>
2017-11-15 18:33:51 -0600

    test git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main
 {
-   public static void main( String[] args )
-   {
-      new Laura().isCute();
-   }
+    //Start here :)
+    public static void main(String[] args)
+    {
+        new Laura().isCute();
+    }
 }
